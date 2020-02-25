a2ebd2bf2acff080ab77038c330c01264c761b68
Patrick Furtak <patrick@patrickfurtak.com>
2020-02-25 10:02:07 -0500

    completed exercise set1

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 /*
 Create a class called Person which accepts the name of a person as a string, and his/her age as a number.
 
-The Person class should have a method called describe which returns a string with the following syntax: "name, age years old". So for example, if John is 19 years old then the function describe of his object will return "John, 19 years old".
+The Person class should have a method called describe which returns a string with the following syntax:
+"name, age years old". So for example, if John is 19 years old then the function describe of his
+object will return "John, 19 years old".
 */
 
-class Person {}
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  sayHello() {
+    console.log(`Hi my name is, ${this.name}`);
+  }
+  describe() {
+    console.log(`${this.name}, is ${this.age} years old.`);
+  }
+}
+
+const me = new Person('Patrick', 30);
+me.sayHello();
+me.describe();
 
 /*
-Create an object called Teacher derived from the Person class, and implement a method called teach which receives a string called subject, and prints out:
+Create an object called Teacher derived from the Person class,
+and implement a method called teach which receives a string called subject, 
+and prints out: Sean teaches WebDev
 */
+
+class Teacher {
+  constructor(name, subject) {
+    this.name = name;
+    this.subject = subject;
+  }
+  describe() {
+    console.log(`${this.name}, teaches ${this.subject}`);
+  }
+}
+const sean = new Teacher('Sean', 'Web Development');
+sean.describe();
